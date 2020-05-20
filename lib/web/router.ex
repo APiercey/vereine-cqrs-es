83ae69ec233b3b2f09c1b2198e349ee3c7060226
Alexander Butt-Piercey <alexander.piercey@gmail.com>
2020-05-21 01:42:05 +0200

    Router template

---
+++ lib/web/router.ex
@@ -0,0 +1,22 @@
+defmodule EventsPoller.Router do
+  use Plug.Router
+
+  plug(:match)
+  plug(:dispatch)
+
+  get("/hcheck", do: send_resp(conn, 200, "200 OK"))
+  get("/favicon.ico", do: send_resp(conn, 200, ""))
+
+  get "/statistics" do
+    data =
+      %{}
+      |> prepare_response
+      |> Poison.encode!()
+
+    conn
+    |> put_resp_content_type("application/json")
+    |> send_resp(200, data)
+  end
+
+  defp prepare_response(data), do: %{data: data}
+end
